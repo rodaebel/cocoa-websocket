69c7f9a39cf9b594385491606d69fcafa5e6f823
rodaebel <tobias.rodaebel@googlemail.com>
2011-06-19 12:27:46 +0200

    More tests.

--- WebSocketTests/WebSocketTests.m
+++ WebSocketTests/WebSocketTests.m
@@ -8,6 +8,20 @@
 #import "WebSocketTests.h"
 
 
+typedef struct SecKey {
+    uint32_t num;
+    NSString *key;
+} SecKey;
+
+
+@interface WebSocket (TestAdditions)
+
+- (struct SecKey)_makeKey;
+- (void)_makeChallengeNumber:(uint32_t)number withBuffer:(unsigned char *)buf;
+
+@end
+
+
 @implementation WebSocketTests
 
 - (void)setUp
@@ -31,6 +45,26 @@
 {
     STAssertThrows([WebSocket webSocketWithURLString:@"http://localhost:8888/" delegate:self],
                    @"Should throw an exception.");
+}
+
+- (void)testWebSocketKeyGeneration
+{
+    SecKey seckey;
+    NSString *key;
+    unsigned char bytes[4];
+
+    seckey = [webSocket _makeKey];
+
+    key = seckey.key;
+
+    STAssertTrue([key isKindOfClass:[NSString class]],
+                 @"Security Key must be a string");
+    STAssertTrue([key characterAtIndex:0] != 13U,
+                 @"First character must not be blank");
+    STAssertTrue([key characterAtIndex:[key length]-1] != 13U,
+                 @"Last character must not be blank");
+
+    [webSocket _makeChallengeNumber:seckey.num withBuffer:&bytes[0]];
 }
 
 # pragma mark - WebSocketDelegate Methods
